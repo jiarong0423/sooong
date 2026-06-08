e914f99fa3936ea8410eab13ec82bdbfd4b14caa
jiarong sun <jiarong0423@gmail.com>
2026-06-09 02:28:35 +0800

    Update Apps Script endpoint

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SOONG_FORM_CONFIG = {
-  googleScriptEndpoint: "https://script.google.com/macros/s/AKfycbzqrHXrbb5rBdt8cC2KpIX6BxjR0Yv4TfqEWibKggb6nQzxNHWqiuvIBgwTIoinSrYW/exec",
+  googleScriptEndpoint: "https://script.google.com/macros/s/AKfycbyVwkWVG7W3--vs2M-ndGbQJNs3BCNnKfW-M4QBUV1BIw7dJB7lwiSJZmOPgJkeFMPN/exec",
   afterSubmitUrl: "",
   source: "github_pages_public_site"
 };
