58c9640dc96791e7315b07ade14af2e45b55d4cb
jiarong sun <jiarong0423@gmail.com>
2026-06-09 02:19:43 +0800

    Configure Apps Script form endpoint

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SOONG_FORM_CONFIG = {
-  googleScriptEndpoint: "",
+  googleScriptEndpoint: "https://script.google.com/macros/s/AKfycbzqrHXrbb5rBdt8cC2KpIX6BxjR0Yv4TfqEWibKggb6nQzxNHWqiuvIBgwTIoinSrYW/exec",
   afterSubmitUrl: "",
   source: "github_pages_public_site"
 };
